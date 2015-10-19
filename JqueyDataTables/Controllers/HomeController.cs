3f2989bdaa38d2f8bf2aeb0a9412d4c87b730d7f
romnyduarte <romnyduarte@outlook.com>
2015-10-19 09:28:03 -0500

    Commit inicial para el post Implementando Jquery DataTables en Asp.Net MVC 5 Part 1.

---
+++ JqueyDataTables/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueyDataTables.Controllers
+{
+    public class HomeController : Controller
+    {
+        private northwindEntities db = new northwindEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Customers()
+        {
+            var customers = db.Customers.ToList();
+            return View(customers);
+        }
+    }
+}
---
+++ JqueyDataTables/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JqueyDataTables.Startup))]
+namespace JqueyDataTables
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
