646e43e8e969d5e0b64b2237814bb8d994acd755
romnyduarte <romnyduarte@outlook.com>
2015-10-27 08:40:32 -0500

    commit del post Implementando Jquery DataTables en Asp.Net MVC 5 Part 2.

--- JqueyDataTables/Controllers/HomeController.cs
+++ JqueyDataTables/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JqueyDataTables.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -29,8 +30,81 @@ namespace JqueyDataTables.Controllers
         }
         public ActionResult Customers()
         {
-            var customers = db.Customers.ToList();
-            return View(customers);
+            return View();
+        }
+        public ActionResult GetDataCustomers(jQueryDataTableParams param)
+        {
+            //Traer registros
+            IQueryable<Customers> memberCol = db.Customers.AsQueryable();
+
+            //Manejador de filtros
+            int totalCount = memberCol.Count();
+            IEnumerable<Customers> filteredMembers = memberCol;
+            if (!string.IsNullOrEmpty(param.sSearch))
+            {
+                filteredMembers = memberCol
+                        .Where(m => m.CompanyName.Contains(param.sSearch) ||
+                           m.ContactName.Contains(param.sSearch) ||
+                           m.ContactTitle.Contains(param.sSearch) ||
+                           m.Address.Contains(param.sSearch) ||
+                           m.City.Contains(param.sSearch) ||
+                           m.Region.Contains(param.sSearch) ||
+                           m.PostalCode.Contains(param.sSearch) ||
+                           m.Country.Contains(param.sSearch) ||
+                           m.Phone.Contains(param.sSearch) ||
+                           m.Fax.Contains(param.sSearch));
+            }
+            //Manejador de orden
+            var sortIdx = Convert.ToInt32(Request["iSortCol_0"]);
+            Func<Customers, string> orderingFunction =
+                                (
+                                m => sortIdx == 0 ? m.CompanyName :
+                                  sortIdx == 1 ? m.ContactName :
+                                  sortIdx == 2 ? m.ContactTitle :
+                                  sortIdx == 3 ? m.Address :
+                                  sortIdx == 4 ? m.City :
+                                  sortIdx == 5 ? m.Region :
+                                  sortIdx == 6 ? m.PostalCode :
+                                  sortIdx == 7 ? m.Country :
+                                  sortIdx == 8 ? m.Phone :
+                                  sortIdx == 9 ? m.Fax :
+                                  m.CustomerID
+                                 );
+            var sortDirection = Request["sSortDir_0"]; // asc or desc  
+            if (sortDirection == "asc")
+                filteredMembers = filteredMembers.OrderBy(orderingFunction);
+            else
+                filteredMembers = filteredMembers.OrderByDescending(orderingFunction);
+            var displayedMembers = filteredMembers
+                     .Skip(param.iDisplayStart)
+                     .Take(param.iDisplayLength);
+
+            //Manejardo de resultados
+            var result = from a in displayedMembers
+                         select new
+                         {
+                             a.CompanyName,
+                             a.ContactName,
+                             a.ContactTitle,
+                             a.Address,
+                             a.City,
+                             a.Region,
+                             a.PostalCode,
+                             a.Country,
+                             a.Phone,
+                             a.Fax
+
+
+                         };
+            //Se devuelven los resultados por json
+            return Json(new
+            {
+                sEcho = param.sEcho,
+                iTotalRecords = totalCount,
+                iTotalDisplayRecords = filteredMembers.Count(),
+                aaData = result
+            },
+               JsonRequestBehavior.AllowGet);
         }
     }
 }
